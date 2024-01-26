a2ef8426a6ceb213b66b331c114e4703a57518f9
catherinehenry <catherinemshenry@gmail.com>
2024-01-25 20:28:38 -0700

    trying mobile same and huggingface sam

--- IAC/iac.py
+++ IAC/iac.py
@@ -10,7 +10,8 @@ os.environ['COZMO'] = "/Users/catherinehenry/Dev/cozmo-python-sdk-fork/src"
 from retico_core.debug import DebugModule
 from retico_dino.dino import Dinov2ObjectFeatures
 from retico_vision.vision import ExtractObjectsModule
-from retico_sam.sam import SAMModule
+from retico_sam.hfsam import SAMModule
+# from retico_sam.sam import SAMModule
 from retico_cozmorobot.cozmo_camera import CozmoCameraModule
 import cozmo
 import tkinter
@@ -27,10 +28,14 @@ def init_all(robot : cozmo.robot.Robot):
     robot.set_head_angle(degrees(10), accel=10.0, max_speed=10.0, duration=1,
                          warn_on_clamp=True, in_parallel=True, num_retries=2).wait_for_completed()
 
-    path_var = ModelCheckpoint.b
+    # path_var = ModelCheckpoint.b
+    # path_var = 'mobile_sam.pt'
 
     cozmo_cam = CozmoCameraModule(robot, exposure=0.45, gain=0.03)
-    sam = SAMModule(model=path_var.name, path_to_chkpnt=path_var.value, use_bbox=True)
+
+    # sam = SAMModule(model=path_var.name, path_to_chkpnt=path_var.value, use_bbox=True) # fb sam
+    # sam = SAMModule(model='t', path_to_chkpnt=path_var, use_bbox=True) # mobile same
+    sam = SAMModule(show=False, use_bbox=True)   # hugging face sam
     extractor = ExtractObjectsModule(num_obj_to_display=1)
     feats = Dinov2ObjectFeatures(show=False, save=True, top_objects=1)
     debug = DebugModule()

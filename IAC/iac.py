e8751389bf1be7d65e3a4aa548f63877433fd360
CatherineHenry <catherinemshenry@gmail.com>
2024-01-27 22:00:10 -0700

    misc formatting

--- IAC/iac.py
+++ IAC/iac.py
@@ -1,17 +1,14 @@
 import sys, os
 from enum import Enum
 
-import time
-
 os.environ['PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION'] = 'python'
 os.environ['COZMO'] = "/Users/catherinehenry/Dev/cozmo-python-sdk-fork/src"
 
 from retico_core.debug import DebugModule
+from retico_vision.vision import WebcamModule
 from retico_dino.dino import Dinov2ObjectFeatures
 from retico_vision.vision import ExtractObjectsModule
 from retico_sam.hfsam import SAMModule
-from retico_vision.vision import WebcamModule
-
 # from retico_sam.sam import SAMModule
 
 
@@ -19,14 +16,11 @@ class ModelCheckpoint(Enum):
     h = 'sam_vit_h_4b8939.pth'  # huge
     l = 'sam_vit_l_0b3195.pth'  # large
     b = 'sam_vit_b_01ec64.pth'  # base
-
-
-
-    # path_var = ModelCheckpoint.b
-    # path_var = 'mobile_sam.pt'
+    
+# path_var = ModelCheckpoint.b
+# path_var = 'mobile_sam.pt'
 
 webcam = WebcamModule()
-
 # sam = SAMModule(model=path_var.name, path_to_chkpnt=path_var.value, use_bbox=True) # fb sam
 # sam = SAMModule(model='t', path_to_chkpnt=path_var, use_bbox=True) # mobile same
 sam = SAMModule(show=False, use_bbox=True)   # hugging face sam
